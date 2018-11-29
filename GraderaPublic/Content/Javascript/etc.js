53540bb5d8eb0c96fbccfd6be567828602f04f5c
Tyre <victor@gradera.nu>
2018-11-29 17:38:29 +0100

    Adding service worker with cache

---
+++ GraderaPublic/Content/Javascript/etc.js
@@ -0,0 +1,76 @@
+﻿$(window).load(function () {
+    $('#slider').nivoSlider({
+        effect: 'random',
+        slices: 15,
+        animSpeed: 700,
+        pauseTime: 2500,
+        startSlide: 0, //Set starting Slide (0 index)
+        directionNav: false,
+        directionNavHide: false, //Only show on hover
+        controlNav: false, //1,2,3...
+        controlNavThumbs: false, //Use thumbnails for Control Nav
+        pauseOnHover: true, //Stop animation while hovering
+        manualAdvance: false, //Force manual transitions
+        captionOpacity: 0.6, //Universal caption opacity
+        beforeChange: function () { },
+        afterChange: function () { },
+        slideshowEnd: function () { } //Triggers after all slides have been shown
+    });
+});
+
+
+function sendFeedback() {
+    $('#valid').slideUp('fast');
+    $.post("../../Home/SendFeedback/", getFeedbackData(), function (data) {
+        if (data == "ok") {
+            $('#valid').html("<p style='color: Green'>Tack för ditt meddelande</p>");
+            $('#valid').slideDown('slow');
+            setTimeout(function () {
+                closeFeedback();
+                $('#name').val("");
+                $('#email').val("");
+                $('#subject').val("");
+                $('#text').val("");
+            }, 4000);
+        }
+        else {
+            $('#valid').html(data);
+            $('#valid').slideDown('slow');
+        }
+    })
+}
+
+function getFeedbackData() {
+    var name = $('#name').val();
+    var email = $('#email').val();
+    var subject = $('#subject').val();
+    var text = $('#text').val();
+
+    return { Name: name, Email: email, Subject: subject, Text: text };
+}
+
+function showFeedback() {
+    $('#feedback').slideDown('slow', function () {
+    });
+    $('#fader').fadeIn('slow', function () {
+    });
+}
+
+function showSwish() {
+    $('#swish').slideDown('slow', function () { });
+    $('#fader').fadeIn('slow', function () { });
+}
+
+function closeSwish() {
+    $('#swish').slideUp('slow', function () {
+    });
+    $('#fader').fadeOut('slow', function () {
+    });
+}
+
+function closeFeedback() {
+    $('#feedback').slideUp('slow', function () {
+    });
+    $('#fader').fadeOut('slow', function () {
+    });
+}
---
+++ GraderaPublic/sw.js
@@ -0,0 +1,40 @@
+﻿self.addEventListener("install", function (e) {
+    console.log('[ServiceWorker] Installed');
+
+    e.waitUntil(
+        caches.open(cacheName).then(function (cache) {
+            console.log('[ServiceWorker] Caching files...');
+            return cache.addAll(cacheFiles);
+        })
+    );
+});
+
+self.addEventListener("activate", function (e) {
+    console.log('[ServiceWorker] Activated');
+
+    e.waitUntil(
+        caches.keys().then(function (cacheNames) {
+            return Promise.all(cacheNames.map(function (thisCacheName) {
+                if (thisCacheName !== cacheName) {
+                    console.log('[ServiceWorker] removing cache files from: ', thisCacheName);
+                    return caches.delete(thisCacheName);
+                }
+            }))
+        })
+    );
+});
+
+self.addEventListener("fetch", function (e) {
+    console.log('[ServiceWorker] Fetched', e.request.url);
+
+    e.respondWith(
+        caches.match(e.request).then(function (response) {
+            if (response) {
+                console.log('[ServiceWorker] found in cache', e.request.url);
+                return response;
+            }
+
+            return fetch(e.request);
+        })
+    )
+});
